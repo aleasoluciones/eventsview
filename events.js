6643f5181085fa250a949ca04421f23fe6cef064
Eduardo Ferro Aldama <eduardo.ferro.aldama@gmail.com>
2015-07-15 14:09:34 +0200

    Integrated events from data and detect fields names and values

--- events.js
+++ events.js
@@ -21,6 +21,7 @@ var EVENTS = {};
   ns.createAjaxRepository = function(url) {
     var events = [];
     var warningEvents = [];
+    var fields = {};
 
     var onUpdateCallback = null;
 
@@ -28,9 +29,28 @@ var EVENTS = {};
         $.get(url, {}, function(data){
           events = JSON.parse(data);
           warningEvents = events.filter(function(e){ return e.warning == true});
+
+          fields = {};
+          for (i=0; i< events.length; i++){
+            for (var property in events[i]) {
+
+              if (property === "message" || property === "id" || property === "timestamp")
+                continue;
+
+              if (! fields[property]) {
+                fields[property] = [];
+              }
+
+              if ( fields[property].indexOf(events[i][property]) == -1 ) {
+                 fields[property].push(events[i][property]);
+              }
+            }
+          }
+          console.log("EFA", fields);
+
           onUpdateCallback && onUpdateCallback();
         });
-    }, 6000);
+    }, 2000);
 
     return {
       findAll: function() {
--- server.py
+++ server.py
@@ -6,61 +6,33 @@ import random
 import json
 import datetime
 import time
+import data
 
-
-events = []
-
-class EventsHandler(tornado.web.RequestHandler):
-    def randomErrorEvent(self):
-        return random.choice([
-          { "message":"Error provision", "warning":True},
-          { "message":"Error al provisionar: Este modelo no soporta TV",
-              "warning":True},
-          { "message":"Error al provisionar: número de líneas no soportado por el modelo", "warning":True},
-            ])
-
-    def randomEvent(self):
-        return  random.choice([
-          { "message":"Cambio datos Telefonía"},
-          { "message":"Cambio de servicio 30Megas, 1 Línea, IPTV a 60Megas, 1 Línea, IPTV"},
-          { "message":"Cambio datos GPON"},
-          { "message":"Línea 1: Registrada"},
-          { "message":"Online alepregpon1 0/1/2/2"},
-          { "message":"DHCP lease 192.168.105.164"},
-          { "message":"Offline"},
-          { "message":"Línea 1: Registrada"},
-          { "message":"ACS OK Firmware upgrade"},
-          { "message":"Línea 1: Registrada"},
-          { "message":"ACS Firmware upgrade requested HG8247T_V200R006C00SPC201_full_all.bin"},
-          { "message":"Línea 1: Registrada"},
-          { "message":"ACS Boot"}])
-
-    def getNewEvents(self, numEvents, nextId):
-        newEvents = []
-        now = datetime.datetime.now()
-        for num in range(numEvents):
-            if num % 25 == 0:
-                event = self.randomErrorEvent()
-            else:
-                event = self.randomEvent()
-            event['timestamp'] = to_timestamp(now + datetime.timedelta(1))
-            event['id'] = nextId + num
-            newEvents.append(event)
-        return newEvents
-
-    def get(self):
-        if len(events)>0:
-            nextId = events[-1]['id'] + 1
-        else:
-            nextId = 0
-        events.extend(self.getNewEvents(50, nextId))
-        self.write(json.dumps(events[-50:]))
 
 
 def to_timestamp(t):
     return time.mktime(t.timetuple()) + (t.microsecond / 1000000.0)
 
+events = []
+now = datetime.datetime.now()
+for idx,e in enumerate(data.events):
+    e["id"] = idx
+    e['timestamp'] = to_timestamp(now + datetime.timedelta(1))
+    events.append(e)
 
+offset=0
+last_access = to_timestamp(now)
+events_to_send=[]
+
+class EventsHandler(tornado.web.RequestHandler):
+    def get(self):
+        global offset
+        now = to_timestamp(datetime.datetime.now())
+        num_events = int(now-last_access)
+        events_to_send.extend(events[offset:offset+num_events])
+        self.write(json.dumps(events_to_send))
+        offset=len(events_to_send)
+        last_acces = now
 
 application = tornado.web.Application([
     (r"/events", EventsHandler),
